516b9329ad689784692902a3248f2abe0a4e973b
lishengning <lsn1183@outlook.com>
2023-12-31 00:10:09 +0800

    update 12-29

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,4 @@
 export const API = 'http://localhost:3001';
-export const CLIENT_API = 'http://localhost:3001';
-
-// export const API = 'http://217.197.161.54:3001';
-// export const CLIENT_API = 'http://217.197.161.54:3001';
+export const CLIENT_API = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'http://217.197.161.54:3001';
+// console.log('process.env', process.env.NODE_ENV);
 
