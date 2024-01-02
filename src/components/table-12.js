01fd1f3cd9e4e5c5a45f4e3aaedfab5d604de28b
lishengning <lsn1183@outlook.com>
2024-01-03 03:28:00 +0800

    update code

--- src/components/table-12.js
+++ src/components/table-12.js
@@ -10,7 +10,7 @@ export default function Table12({ title, data }) {
     periods = periods < 10 ? '00' + periods : periods < 100 ? '0' + periods : periods
     return { ...item, ...openItem, periods }
   })
-  console.log('data', list);
+  // console.log('data', list);
   return (
     <div className="w-full">
       <div
