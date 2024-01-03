3aed100e2f8fe2503d8d1ca679f5d50639830416
lishengning <lsn1183@outlook.com>
2024-01-03 16:53:01 +0800

    update code

--- src/components/CountDown/numberbox.js
+++ src/components/CountDown/numberbox.js
@@ -1,5 +1,6 @@
 // bg-[#343650]
 export const NumberBox = ({ num, unit, flip }) => {
+  console.log('num', num);
   return (
     <div className="flex flex-col items-center">
       <div className="relative flex flex-col items-center justify-center rounded-lg w-6 h-6">
