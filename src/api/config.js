1dd62ef7d1337790f6ce9222f79d3e7ced215ed9
lishengning <lsn1183@outlook.com>
2024-01-03 22:39:49 +0800

    update code

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,7 @@
 export const API = 'http://localhost:3001';
-export const CLIENT_API = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'http://217.197.161.54:3001';
+export const CLIENT_API = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://leihou360.xyz/api';
 // console.log('process.env', process.env.NODE_ENV);
 
 export const Title = '欧洲彩';
 export const openTime = '17:35';
+
